025b916310b84a9f8c1b7ac347e92463be842a0c
William Talbot <william.talbot97@gmail.com>
2024-07-10 12:57:34 +0200

    added matrix properties functions

--- include/mathbox/impl/matrix_properties.hpp
+++ include/mathbox/impl/matrix_properties.hpp
@@ -6,8 +6,34 @@
 namespace math {
 
 template<typename Derived>
+bool has_positive_diagonals(const Eigen::MatrixBase<Derived>& m) {
+    return m.diagonal().minCoeff() > 0.0;
+}
+
+template<typename Derived>
+bool is_positive_definite(const typename Eigen::EigenBase<Derived>& m) {
+    if (!is_symmetric(m)) {
+        return false;
+    }
+    typename Eigen::LLT<Derived> llt(m);  // compute the Cholesky decomposition
+    return llt.info() == Eigen::Success;
+}
+
+template<typename Derived>
 inline bool is_skew_symmetric(const Eigen::DenseBase<Derived>& m) {
     return m.isApprox(-m.transpose());
+}
+
+template<typename Derived>
+bool is_symmetric(const Eigen::DenseBase<Derived>& m, const typename Derived::Scalar precision) {
+    for (Eigen::Index r = 0; r < m.rows() - 1; ++r) {
+        for (Eigen::Index c = r + 1; c < m.cols(); ++c) {
+            if (std::abs(m(r, c) - m(c, r)) > precision) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 }
--- include/mathbox/matrix_properties.hpp
+++ include/mathbox/matrix_properties.hpp
@@ -7,6 +7,29 @@
 namespace math {
 
 /**
+ * @brief Check if matrix has all diagonals positive (strictly greater than zero).
+ * 
+ * @tparam Derived 
+ * @param m 
+ * @return true 
+ * @return false 
+ */
+template<typename Derived>
+bool has_positive_diagonals(const Eigen::MatrixBase<Derived>& m);
+
+/**
+ * @brief Check if matrix is positive-definite. This function first checks if the matrix is symmetric, before checking
+ * for positive-definiteness through Cholesky decomposition.
+ *
+ * @tparam Derived
+ * @param m
+ * @return true
+ * @return false
+ */
+template<typename Derived>
+bool is_positive_definite(const Eigen::EigenBase<Derived>& m);
+
+/**
  * @brief Check if matrix is skew-symmetric.
  *
  * @tparam Derived
@@ -16,6 +39,19 @@ namespace math {
  */
 template<typename Derived>
 bool is_skew_symmetric(const Eigen::DenseBase<Derived>& m);
+
+/**
+ * @brief Check if a matrix is symmetric about its diagonal, up to some level of precision (default = exact).
+ * 
+ * @tparam Derived 
+ * @param m 
+ * @param precision 
+ * @return true 
+ * @return false 
+ */
+template<typename Derived>
+bool is_symmetric(const Eigen::DenseBase<Derived>& m,
+        const typename Derived::Scalar precision = static_cast<typename Derived::Scalar>(0));
 
 }
 
