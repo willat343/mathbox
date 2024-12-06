2af78e74209144b4281ad331d2e4a9f3a9ca55a4
William Talbot <william.talbot97@gmail.com>
2024-12-06 12:33:25 +0100

    updated lerp and linear function to support times and durations

--- mathbox/include/mathbox/impl/lerp.hpp
+++ mathbox/include/mathbox/impl/lerp.hpp
@@ -1,22 +1,47 @@
 #ifndef MATHBOX_IMPL_LERP_HPP
 #define MATHBOX_IMPL_LERP_HPP
 
+#include <chrono>
+#include <stdexcept>
+#include <type_traits>
+
 #include "mathbox/lerp.hpp"
+#include "mathbox/time.hpp"
+#include "mathbox/traits.hpp"
 
 namespace math {
 
-template<typename ArithmeticType, typename Scalar>
-inline ArithmeticType lerp(const ArithmeticType& y_0, const ArithmeticType& y_1, const Scalar alpha) {
-    return (static_cast<Scalar>(1) - alpha) * y_0 + alpha * y_1;
+template<typename T, typename Scalar>
+inline T lerp(const T& y_0, const T& y_1, const Scalar alpha) {
+    static_assert(is_arithmetic_type_v<T> || is_time_point_v<T> || is_duration_v<T>,
+            "Type must be ArithmeticType, std::chrono::time_point<Clock, Duration> or std::chrono::duration<Rep, "
+            "Period>.");
+    if constexpr (is_arithmetic_type_v<T>) {
+        return (static_cast<Scalar>(1) - alpha) * y_0 + alpha * y_1;
+    } else if constexpr (is_time_point_v<T>) {
+        return y_0 + std::chrono::duration_cast<typename T::duration>(alpha * (y_1 - y_0));
+    } else if constexpr (is_duration_v<T>) {
+        return std::chrono::duration_cast<T>((static_cast<Scalar>(1) - alpha) * y_0 + alpha * y_1);
+    }
 }
 
-template<typename ArithmeticType, typename Scalar>
-inline ArithmeticType linear_function(const Scalar x, const Scalar x_0, const Scalar x_1, const ArithmeticType y_0,
-        const ArithmeticType y_1) {
+template<typename ArithmeticType, typename IndependentVariableType>
+inline ArithmeticType linear_function(const IndependentVariableType x, const IndependentVariableType x_0,
+        const IndependentVariableType x_1, const ArithmeticType y_0, const ArithmeticType y_1) {
+    static_assert(is_arithmetic_type_v<ArithmeticType>, "ArithmeticType must be arithmetic type.");
+    static_assert(std::is_arithmetic_v<IndependentVariableType> || is_time_point_v<IndependentVariableType>,
+            "IndependentVariableType must be Scalar or std::chrono::time_point<Clock, Duration>.");
     if (x_0 == x_1) {
         throw std::runtime_error("Divide by zero encountered in linear_function because x_0 == x_1.");
     }
-    return lerp(y_0, y_1, (x - x_0) / (x_1 - x_0));
+    if constexpr (std::is_arithmetic_v<IndependentVariableType>) {
+        return lerp(y_0, y_1, (x - x_0) / (x_1 - x_0));
+    } else if constexpr (is_time_point_v<IndependentVariableType>) {
+        // Cast is required to avoid integer division.
+        using Scalar = ArithmeticTypeTraits<ArithmeticType>::Scalar;
+        return lerp(y_0, y_1,
+                to_sec<decltype(x - x_0), Scalar>(x - x_0) / to_sec<decltype(x_1 - x_0), Scalar>(x_1 - x_0));
+    }
 }
 
 }
--- mathbox/include/mathbox/lerp.hpp
+++ mathbox/include/mathbox/lerp.hpp
@@ -1,8 +1,6 @@
 #ifndef MATHBOX_LERP_HPP
 #define MATHBOX_LERP_HPP
 
-#include "mathbox/traits.hpp"
-
 namespace math {
 
 /**
@@ -15,15 +13,25 @@ namespace math {
  *
  * If \f$\alpha \in [0, 1]\f$, the function performs interpolation, and otherwise it extrapolates.
  *
- * @tparam ArithmeticType must support multiplication with Scalar, and addition through the + operator
- * @tparam Scalar must be a floating point type
+ * To reduce floating point inaccuracies ("catastrophic cancellation" when the input
+ * values are large), this formula is preferred over:
+ *
+ * \f[
+ *      \mathbf{y} = \mathbf{y} = \mathbf{y}_0 + \alpha (\mathbf{y}_1 - \mathbf{y}_0)
+ * \f]
+ *
+ * This second formula is used for the linear interpolation of `std::chrono::time_point<Clock, Duration>` objects.
+ *
+ * @tparam T This must be a mathbox arithmetic type (e.g., floating-point scalars and Eigen matrices), a
+ * `std::chrono::time_point<Clock, Duration>` or std::chrono::duration<Rep, Period>` object.
+ * @tparam Scalar
  * @param y_0 start point (\f$\alpha = 0\f$) of interpolation \f$\mathbf{y}_0\f$
  * @param y_1 end point (\f$\alpha = 1\f$) of interpolation \f$\mathbf{y}_1\f$
  * @param alpha interpolation or extrapolation factor \f$\alpha\f$
- * @return ArithmeticType interpolation or extrapolation result \f$\mathbf{y}\f$
+ * @return T interpolation or extrapolation result \f$\mathbf{y}\f$
  */
-template<typename ArithmeticType, typename Scalar = typename ArithmeticTypeTraits<ArithmeticType>::Scalar>
-ArithmeticType lerp(const ArithmeticType& y_0, const ArithmeticType& y_1, const Scalar alpha);
+template<typename T, typename Scalar>
+T lerp(const T& y_0, const T& y_1, const Scalar alpha);
 
 /**
  * @brief Linear function which passes through \f$(x_0, y_0)\f$ and \f$(x_1, y_1)\f$. The function returns `lerp(y_0,
@@ -34,7 +42,7 @@ ArithmeticType lerp(const ArithmeticType& y_0, const ArithmeticType& y_1, const 
  * \f]
  *
  * @tparam ArithmeticType
- * @tparam Scalar
+ * @tparam IndependentVariableType
  * @param x
  * @param x_0
  * @param x_1
@@ -42,9 +50,9 @@ ArithmeticType lerp(const ArithmeticType& y_0, const ArithmeticType& y_1, const 
  * @param y_1
  * @return ArithmeticType
  */
-template<typename ArithmeticType, typename Scalar = typename ArithmeticTypeTraits<ArithmeticType>::Scalar>
-inline ArithmeticType linear_function(const Scalar x, const Scalar x_0, const Scalar x_1, const ArithmeticType y_0,
-        const ArithmeticType y_1);
+template<typename ArithmeticType, typename IndependentVariableType>
+inline ArithmeticType linear_function(const IndependentVariableType x, const IndependentVariableType x_0,
+        const IndependentVariableType x_1, const ArithmeticType y_0, const ArithmeticType y_1);
 
 }
 
