2ab349a7d3089c38dfad96cdf089faac83358e1e
William Talbot <william.talbot97@gmail.com>
2026-01-14 16:34:22 +0100

    added matrix diagnostics functions

---
+++ include/mathbox/impl/matrix_diagnostics.hpp
@@ -0,0 +1,222 @@
+#ifndef MATHBOX_IMPL_MATRIX_DIAGNOSTICS_HPP
+#define MATHBOX_IMPL_MATRIX_DIAGNOSTICS_HPP
+
+#include <algorithm>
+#include <cppbox/parse.hpp>
+#include <sstream>
+#include <utility>
+
+#include "mathbox/matrix_diagnostics.hpp"
+
+namespace math {
+
+template<typename Derived>
+std::string matrix_type_and_size(const Eigen::MatrixBase<Derived>& m) {
+    std::stringstream ss;
+    if (m.isZero()) {
+        ss << "0";
+    } else if (m.isIdentity()) {
+        ss << "I";
+    } else {
+        if (Eigen::NumTraits<typename Derived::Scalar>::IsInteger) {
+            ss << "Z";
+        } else if (Eigen::NumTraits<typename Derived::Scalar>::IsComplex) {
+            ss << "C";
+        } else {
+            ss << "R";
+        }
+        if (!Eigen::NumTraits<typename Derived::Scalar>::IsSigned) {
+            ss << "^+";
+        }
+    }
+    ss << "_" << m.rows() << "x" << m.cols();
+    return ss.str();
+}
+
+template<typename Derived>
+std::string structure_diagnostics(const Eigen::MatrixBase<Derived>& m, const std::vector<std::size_t>& row_block_sizes,
+        const std::vector<std::size_t>& column_block_sizes,
+        const std::optional<std::vector<std::string>>& row_block_names,
+        const std::optional<std::vector<std::string>>& column_block_names,
+        const std::optional<std::string>& matrix_name) {
+    // Check validity of inputs
+    assert(!row_block_names || row_block_sizes.size() == row_block_names->size());
+    assert(!column_block_names || column_block_sizes.size() == column_block_names->size());
+    assert(std::count(row_block_sizes.begin(), row_block_sizes.end(), 0) > 0 ||
+            std::accumulate(row_block_sizes.begin(), row_block_sizes.end(), 0) == m.rows());
+    assert(std::count(column_block_sizes.begin(), column_block_sizes.end(), 0) > 0 ||
+            std::accumulate(column_block_sizes.begin(), column_block_sizes.end(), 0) == m.cols());
+
+    // Generate diagnostic information
+    std::stringstream ss;
+    if (m.size() > 0) {
+        // Precompute min/max string lengths
+        const std::size_t max_row_size_width = std::to_string(
+                row_block_sizes.empty() ? 0 : *std::max_element(row_block_sizes.cbegin(), row_block_sizes.cend()))
+                                                       .size();
+        const std::size_t min_column_width = 4 + 2 * max_row_size_width;
+
+        // Row identifiers
+        std::vector<std::string> row_identifiers(row_block_sizes.size());
+        for (std::size_t i = 0; i < row_block_sizes.size(); ++i) {
+            std::stringstream internal_ss;
+            internal_ss << (row_block_names ? (*row_block_names)[i] : "Block") << "(" << row_block_sizes[i] << ")";
+            row_identifiers[i] = internal_ss.str();
+        }
+        const std::size_t max_row_identifier_width = cppbox::max_size(row_identifiers);
+
+        // Column identifiers
+        std::vector<std::string> column_identifiers(column_block_sizes.size());
+        for (std::size_t i = 0; i < column_block_sizes.size(); ++i) {
+            std::stringstream internal_ss;
+            internal_ss << (column_block_names ? (*column_block_names)[i] : "Block") << "(" << column_block_sizes[i]
+                        << ")";
+            column_identifiers[i] = internal_ss.str();
+        }
+
+        // Column header
+        std::stringstream column_header_ss;
+        column_header_ss << std::string(max_row_size_width + 3 + max_row_identifier_width, ' ');
+        for (std::size_t i = 0; i < column_block_sizes.size(); ++i) {
+            column_header_ss << " | " << column_identifiers[i];
+        }
+        column_header_ss << " |\n";
+        const std::string column_header = column_header_ss.str();
+
+        // Header
+        const std::string header_start = "=== " + matrix_name.value_or("") + "Matrix Structure ";
+        ss << header_start << std::string(column_header.size() - header_start.size() - 1, '=') << "\n";
+        ss << column_header;
+
+        // Information for each block
+        for (std::size_t i = 0, r = 0; i < row_block_sizes.size(); ++i, r += row_block_sizes[i]) {
+            ss << std::setw(max_row_size_width) << i << " | " << std::setw(max_row_identifier_width)
+               << row_identifiers[i];
+            for (std::size_t j = 0, c = 0; j < column_block_sizes.size(); ++j, c += column_block_sizes[j]) {
+                const Eigen::Ref<const Eigen::MatrixXd>& block =
+                        m.block(r, c, row_block_sizes[i], column_block_sizes[j]);
+                ss << " | " << std::setw(std::max(min_column_width, column_identifiers[j].size()))
+                   << matrix_type_and_size(block);
+            }
+            ss << " |\n";
+        }
+        ss << std::string(column_header.size() - 1, '=') << "\n";
+    }
+    return ss.str();
+}
+
+template<IsVector EigenvaluesDerived, IsMatrix EigenvectorsDerived>
+    requires(std::is_same_v<typename EigenvaluesDerived::Scalar, typename EigenvectorsDerived::Scalar>)
+std::string spectral_structure_diagnostics(const Eigen::MatrixBase<EigenvaluesDerived>& eigenvalues,
+        const Eigen::MatrixBase<EigenvectorsDerived>& eigenvectors,
+        const typename EigenvaluesDerived::Scalar nullspace_threshold,
+        const typename EigenvaluesDerived::Scalar weak_threshold,
+        const typename EigenvaluesDerived::Scalar contribution_threshold,
+        const std::optional<std::vector<std::size_t>>& block_sizes,
+        const std::optional<std::vector<std::string>>& block_names, const std::optional<std::string>& matrix_name,
+        const bool exclude_nullspaces, const bool exclude_weak, const bool exclude_strong) {
+    using Scalar = typename EigenvaluesDerived::Scalar;
+    // Check validity of inputs
+    assert(eigenvalues.size() == eigenvectors.rows());
+    assert(eigenvectors.rows() == eigenvectors.cols());
+    assert(!block_sizes || std::count(block_sizes->begin(), block_sizes->end(), 0) > 0 ||
+            std::accumulate(block_sizes->begin(), block_sizes->end(), 0) == eigenvalues.size());
+    assert(!block_names || (block_sizes && block_sizes->size() == block_names->size()));
+    assert(nullspace_threshold >= static_cast<Scalar>(0));
+    assert(weak_threshold >= nullspace_threshold);
+    assert(contribution_threshold >= static_cast<Scalar>(0) && contribution_threshold <= static_cast<Scalar>(1));
+
+    // Count eigenvalues in each category
+    const int num_nullspaces = (eigenvalues < nullspace_threshold).count();
+    const int num_weak = (eigenvalues < weak_threshold).count() - num_nullspaces;
+    const int num_strong = eigenvalues.size() - num_weak - num_nullspaces;
+
+    // Generate diagnostic information
+    std::stringstream ss;
+    if (eigenvalues.size() > 0) {
+        // Header
+        ss << std::scientific << std::setprecision(3) << "=== " << matrix_name.value_or("")
+           << " Spectral Structure [Max Eigenvalue: " << eigenvalues.maxCoeff()
+           << ", Min Eigenvalue: " << eigenvalues.minCoeff() << ", " << num_strong << " Strong"
+           << ", " << num_weak << " Weak (Threshold: " << weak_threshold << ")"
+           << ", " << num_nullspaces << " Nullspaces (Threshold: " << nullspace_threshold << ")] ===\n";
+        const std::size_t header_width = ss.str().size() - 1;
+        const std::size_t max_index_width = std::to_string(eigenvalues.size() - 1).size();
+
+        // Information for each eigenvalue
+        for (int i = 0; i < eigenvalues.size(); ++i) {
+            // Eigenvalue and Eigenvector
+            const Scalar eigenvalue = eigenvalues[i];
+            const Eigen::Ref<const Eigen::Vector<Scalar, EigenvectorsDerived::RowsAtCompileTime>>& eigenvector =
+                    eigenvectors.col(i);
+
+            // Get significant contributors of eigenvector
+            std::vector<std::pair<std::string, Scalar>> contributions;
+            std::size_t block_index{0};
+            for (int j = 0; j < eigenvector.size(); j += block_sizes ? (*block_sizes)[block_index] : 1, ++block_index) {
+                const Eigen::Ref<const Eigen::Vector<Scalar, Eigen::Dynamic>> eigenvector_block =
+                        eigenvector.segment(j, block_sizes ? (*block_sizes)[block_index] : 1);
+                const Scalar block_contribution = eigenvector_block.norm();
+                if (block_contribution > contribution_threshold) {
+                    std::stringstream internal_ss;
+                    internal_ss << std::fixed << std::setprecision(3) << block_contribution;
+                    internal_ss << " "
+                                << (block_names && !(*block_names)[block_index].empty()
+                                                   ? (*block_names)[block_index]
+                                                   : (block_sizes ? "Block[" + std::to_string(block_index) + "](" +
+                                                                             std::to_string(
+                                                                                     (*block_sizes)[block_index]) +
+                                                                             ")"
+                                                                  : "Element[" + std::to_string(j) + "]"));
+                    if (block_sizes) {
+                        internal_ss << " [" << eigenvector_block.transpose() << "]";
+                    }
+                    contributions.emplace_back(internal_ss.str(), block_contribution);
+                }
+            }
+
+            // Sort eigenvector contributors by significance
+            std::sort(contributions.begin(), contributions.end(),
+                    [](const std::pair<std::string, Scalar>& lhs, const std::pair<std::string, Scalar>& rhs) {
+                        return lhs.second > rhs.second;
+                    });
+
+            // Generate diagnostic information
+            std::stringstream internal_ss;
+            internal_ss << std::scientific << std::setprecision(3) << std::setw(max_index_width) << i << " | "
+                        << std::setw(9)
+                        << (eigenvalue < nullspace_threshold ? "Nullspace"
+                                                             : (eigenvalue < weak_threshold ? "Weak" : "Strong"))
+                        << " | Eigenvalue: " << std::setw(9) << eigenvalue << " | Eigenvector contributors: ";
+            const std::size_t length_to_contributors = internal_ss.str().size();
+            for (std::size_t j = 0; j < contributions.size(); ++j) {
+                internal_ss << (j > 0 ? ",\n" + std::string(length_to_contributors, ' ') : "")
+                            << contributions[j].first;
+            }
+            if (contributions.size() < static_cast<std::size_t>(eigenvector.size())) {
+                internal_ss << (contributions.size() == 0 ? "None" : "")
+                            << ",\n" + std::string(length_to_contributors, ' ') + "Remainder=" << std::fixed
+                            << std::sqrt(static_cast<Scalar>(1) -
+                                         std::accumulate(contributions.cbegin(), contributions.cend(),
+                                                 static_cast<Scalar>(0),
+                                                 [](const Scalar squared_norm,
+                                                         const std::pair<std::string, Scalar>& eigenvector_component) {
+                                                     return squared_norm +
+                                                            eigenvector_component.second * eigenvector_component.second;
+                                                 }));
+            }
+            internal_ss << "\n";
+            if ((eigenvalue < nullspace_threshold && !exclude_nullspaces) ||
+                    (eigenvalue >= nullspace_threshold && eigenvalue < weak_threshold && !exclude_weak) ||
+                    (eigenvalue >= weak_threshold && !exclude_strong)) {
+                ss << internal_ss.str();
+            }
+        }
+        ss << std::string(header_width, '=');
+    }
+    return ss.str();
+}
+
+}
+
+#endif
---
+++ include/mathbox/matrix_diagnostics.hpp
@@ -0,0 +1,88 @@
+#ifndef MATHBOX_MATRIX_DIAGNOSTICS_HPP
+#define MATHBOX_MATRIX_DIAGNOSTICS_HPP
+
+#include <Eigen/Core>
+#include <optional>
+#include <string>
+#include <type_traits>
+
+#include "mathbox/traits.hpp"
+
+namespace math {
+
+/**
+ * @brief Produce diagnostic string about the type and size of a matrix. The type is one of:
+ *  - Z: integer (additional "^+" symbol indicates the type is unsigned)
+ *  - C: complex
+ *  - R: real
+ *  - 0 (special case): zero matrix
+ *  - I (special case): identity matrix
+ *
+ * The size is appended in form "_NxM".
+ *
+ * @tparam Derived
+ * @param m
+ * @return std::string
+ */
+template<typename Derived>
+std::string matrix_type_and_size(const Eigen::MatrixBase<Derived>& m);
+
+/**
+ * @brief Produce diagnostic information about the structure of a matrix.
+ *
+ * @tparam Derived
+ * @param m
+ * @param row_block_sizes
+ * @param column_block_sizes
+ * @param row_block_names
+ * @param column_block_names
+ * @return std::string
+ */
+template<typename Derived>
+std::string structure_diagnostics(const Eigen::MatrixBase<Derived>& m, const std::vector<std::size_t>& row_block_sizes,
+        const std::vector<std::size_t>& column_block_sizes,
+        const std::optional<std::vector<std::string>>& row_block_names = std::nullopt,
+        const std::optional<std::vector<std::string>>& column_block_names = std::nullopt,
+        const std::optional<std::string>& matrix_name = std::nullopt);
+
+/**
+ * @brief Produce diagnostic information about the spectral structure of a square matrix given its eigen decomposition.
+ *
+ * Eigenvector directions are categorised as nullspace, weak, or strong, and their connection with specific elements (or
+ * optionally blocks) is reported.
+ *
+ * @tparam EigenvaluesDerived vector type
+ * @tparam EigenvectorsDerived matrix type
+ * @param eigenvalues eigenvalues
+ * @param eigenvectors eigenvectors, which must be square equal in dimensions to the eigenvalues
+ * @param nullspace_threshold threshold for detecting null directions, e.g. `1.0e-9 * eigenvalues.maxCoeff()`
+ * @param weak_threshold threshold for detecting weak directions, e.g. `1.0e-4 * eigenvalues.maxCoeff()`
+ * @param contribution_threshold threshold for an eigenvector element or block norm to be considered a contributor which
+ * must be in range [0.0, 1.0], e.g. values in [0.01, 0.1] are typical
+ * @param block_sizes optional semantic block sizes, which must sum to the total size (i.e., number of eigenvectors) and
+ * cannot have zero elements
+ * @param block_names optional semantic block names, which if defined requires that `block_sizes` be defined and must be
+ * equal in size to block sizes
+ * @param matrix_name optional matrix name
+ * @param exclude_nullspaces if true, exclude nullspaces from reported diagnostics
+ * @param exclude_weak if true, exclude weak directions from reported diagnostics
+ * @param exclude_strong if true, exclude strong directions from reported diagnostics
+ * @return std::string diagnostic information about the nullspace structure
+ */
+template<IsVector EigenvaluesDerived, IsMatrix EigenvectorsDerived>
+    requires(std::is_same_v<typename EigenvaluesDerived::Scalar, typename EigenvectorsDerived::Scalar>)
+std::string spectral_structure_diagnostics(const Eigen::MatrixBase<EigenvaluesDerived>& eigenvalues,
+        const Eigen::MatrixBase<EigenvectorsDerived>& eigenvectors,
+        const typename EigenvaluesDerived::Scalar nullspace_threshold,
+        const typename EigenvaluesDerived::Scalar weak_threshold,
+        const typename EigenvaluesDerived::Scalar contribution_threshold,
+        const std::optional<std::vector<std::size_t>>& block_sizes = std::nullopt,
+        const std::optional<std::vector<std::string>>& block_names = std::nullopt,
+        const std::optional<std::string>& matrix_name = std::nullopt, const bool exclude_nullspaces = false,
+        const bool exclude_weak = false, const bool exclude_strong = false);
+
+}
+
+#include "mathbox/impl/matrix_diagnostics.hpp"
+
+#endif
