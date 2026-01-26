2b2a7357fa690d91b64a66be8707d0641d52336d
William Talbot <william.talbot97@gmail.com>
2026-01-26 19:55:19 +0100

    fixed function name change in test

--- test/src/vector_operations.cpp
+++ test/src/vector_operations.cpp
@@ -69,7 +69,7 @@ TEST(vector_operations, lin_spaced_4) {
 
 TEST(vector_operations, range_0) {
     const double start{0.0}, end{4.0}, step{1.0};
-    const Eigen::VectorXd result = math::range(step, start, end);
+    const Eigen::VectorXd result = math::range_vector(step, start, end);
     const int expected_size{5};
     const double expected_step = step;
     Eigen::VectorXd expected(expected_size);
@@ -82,7 +82,7 @@ TEST(vector_operations, range_0) {
 
 TEST(vector_operations, range_1) {
     const double start{1.0}, end{5.1}, step{1.0};
-    const Eigen::VectorXd result = math::range(step, start, end);
+    const Eigen::VectorXd result = math::range_vector(step, start, end);
     const int expected_size{5};
     const double expected_step = step;
     Eigen::VectorXd expected(expected_size);
@@ -95,7 +95,7 @@ TEST(vector_operations, range_1) {
 
 TEST(vector_operations, range_2) {
     const double start{1.3}, end{11.2}, step{2.0};
-    const Eigen::VectorXd result = math::range(step, start, end);
+    const Eigen::VectorXd result = math::range_vector(step, start, end);
     const int expected_size{5};
     const double expected_step = step;
     Eigen::VectorXd expected(expected_size);
@@ -108,7 +108,7 @@ TEST(vector_operations, range_2) {
 
 TEST(vector_operations, range_3) {
     const double start{-1.3}, end{-11.2}, step{-2.0};
-    const Eigen::VectorXd result = math::range(step, start, end);
+    const Eigen::VectorXd result = math::range_vector(step, start, end);
     const int expected_size{5};
     const double expected_step = step;
     Eigen::VectorXd expected(expected_size);
@@ -121,7 +121,7 @@ TEST(vector_operations, range_3) {
 
 TEST(vector_operations, range_4) {
     const double start{-11.2}, end{-1.3}, step{2.0};
-    const Eigen::VectorXd result = math::range(step, start, end);
+    const Eigen::VectorXd result = math::range_vector(step, start, end);
     const int expected_size{5};
     const double expected_step = step;
     Eigen::VectorXd expected(expected_size);
